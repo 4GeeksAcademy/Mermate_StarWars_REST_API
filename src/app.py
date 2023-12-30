e714034951c7a50f239f35ad61355424d002972b
Mermate <138124998+Mermate@users.noreply.github.com>
2023-12-30 17:09:02 +0000

    API

--- src/app.py
+++ src/app.py
@@ -36,18 +36,180 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-#INICIO DEL CÓDIGO
+
+
+# [GET]Listar todos los usuarios.
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_user():
+    all_users=User.query.all()
+    results= list( map( lambda user:user.serialize(), all_users ))
+ 
+  
+    return jsonify( results), 200
+   
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+#  ['GET']Listar todos los Characters.
 
-    #FIN DEL CODIGO
+@app.route('/Characters', methods=['GET'])
+def get_Character():
+    all_Characters=Characters.query.all()
+    results= list( map( lambda Characters:Characters.serialize(), all_Characters ))
+    
+    return jsonify( results), 200
+
+#  ['GET']Listar la información de un solo Character
+
+@app.route('/Character/<int:Character_id>', methods=['GET'])
+def get_person(Character_id):
+    person = Characters.query.get(Character_id)
+
+    
+    return jsonify(person.serialize()), 200
+
+#  ['GET'] Listar todos los favoritos.
+
+@app.route('/fav_characters', methods=['GET'])
+def get_fav_characters():
+    fav_characters = Fav_Characters.query.all()
+    results = list(map(lambda Characters: Characters.serialize(), fav_characters))
+
+    return jsonify(results), 200
+
+
+#  ['GET']Listar los registros de planets en la base de datos
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    all_planets=Planets.query.all()
+    results= list( map( lambda Planets:Planets.serialize(), all_planets ))
+    
+    return jsonify( results), 200
+
+#  ['GET'] Listar la información de un solo planet
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planets.query.get(planet_id)
+
+
+    return jsonify(planet.serialize()), 200
+
+
+#^[POST] añade un nuevo character
+
+@app.route('/characters', methods=['POST'])
+def add_new_character():
+
+    request_body_character = request.get_json()
+
+    new_character = Characters(
+        name=request_body_character["name"],
+        mass=request_body_character["mass"],
+        height=request_body_character["height"],
+        hair_color=request_body_character["hair_color"],
+        skin_color=request_body_character["skin_color"], 
+        eye_color=request_body_character["eye_color"], 
+        birth_year=request_body_character["birth_year"],
+         gender=request_body_character["gender"],
+           )
+    db.session.add(new_character)
+    db.session.commit()
+
+    return jsonify(request_body_character), 200
+
+#^[POST] añade un nuevo planeta 
+
+@app.route('/planets', methods=['POST'])
+def add_new_planet():
+
+    body = request.get_json()
+    
+    new_planet = Planets(
+        name=body["name"],
+        population=body["population"],
+        terrain=body["terrain"],
+        climate=body["climate"],
+        diameter=body["diameter"]
+    )
+    
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return jsonify(body), 200
+
+# [POST] Añade una nuevo Character favorito.
+
+@app.route('/fav_characters', methods=['POST'])
+def add_new_fav_character():
+
+    request_body_fav_character = request.get_json()
+
+    new_fav_character = Fav_Characters(
+    character=request_body_fav_character["character"], 
+    user=request_body_fav_character["user"]
+    )
+    db.session.add(new_fav_character)
+    db.session.commit()
+
+    return jsonify(request_body_fav_character), 200
+
+# [POST] Añade un nuevo planet favorito .
+
+@app.route('/fav_planets', methods=['POST'])
+def add_new_fav_planet():
+
+    request_body_fav_planet = request.get_json()
+
+    new_fav_planet = Fav_Planets(
+    planet=request_body_fav_planet["planet"], 
+    user=request_body_fav_planet["user"])
+
+    db.session.add(new_fav_planet)
+    db.session.commit()
+
+    return jsonify(request_body_fav_planet), 200
+
+# [DELETE] Elimina una Character favorito.
+
+@app.route('/fav_characters/<int:fav_characters_id>', methods=['DELETE'])
+def delete_fav_character(fav_characters_id):
+    fav_character = Fav_Characters.query.get(fav_characters_id)
+
+    if not fav_character:
+        return jsonify({'message': 'Fav Character not found'}), 404
+
+    db.session.delete(fav_character)
+    db.session.commit()
+
+    return jsonify({'message': f'Fav character with ID {fav_characters_id} deleted successfully'}), 200
+
+# [DELETE] Elimina un planet favorito.
+
+@app.route('/fav_planets/<int:fav_planets_id>', methods=['DELETE'])
+def delete_fav_planet(fav_planets_id):
+    fav_planet = Fav_Planets.query.get(fav_planets_id)
+
+    if not fav_planet:
+        return jsonify({'message': 'Fav planet not found'}), 404
+
+    db.session.delete(fav_planet)
+    db.session.commit()
+
+    return jsonify({'message': f'Fav planet with ID {fav_planets_id} deleted successfully'}), 200
+
+
+
+
+
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
